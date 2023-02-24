33b992b2a68bc818ecbfde1f92b4c9c0142d1cbe
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-24 10:57:44 -0300

    feat: function

--- 01-app/src/App.js
+++ 01-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function App(){
-  const legume = 'batata';
+  const legume = () => {
+    return 'batata'
+  }
   const itIs = 'Melhor Legume';
   return(
     <section>
-      <h1>{legume}</h1>
+      <h1>{`A ${legume()}`}</h1>
       <h2>{itIs}</h2>
     </section>
   )
