aaf08c2e5d892ff4def7b1b41e6d3d4a50092ef9
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-24 10:56:12 -0300

    feat: js

--- 01-app/src/App.js
+++ 01-app/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function App(){
+  const legume = 'batata';
+  const itIs = 'Melhor Legume';
   return(
-    <>
-    <p>Começando o app</p>
-    </>
+    <section>
+      <h1>{legume}</h1>
+      <h2>{itIs}</h2>
+    </section>
   )
 }
